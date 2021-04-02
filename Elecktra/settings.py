d3c962813097e429ba01c8849d5c9206a68d6eb1
giridhar <giridhar7889@gmail.com>
2021-04-02 23:23:17 +0530

    added chatconfig to installed apps

--- Elecktra/settings.py
+++ Elecktra/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'chat',
+    'chat.apps.ChatConfig',
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
