008210d8b4e37ece694db1ab3555a6bf93e86494
giridhar <giridhar7889@gmail.com>
2021-04-02 23:05:09 +0530

    sorted db for chat

--- electricity/views.py
+++ electricity/views.py
@@ -7,117 +7,131 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from django.conf import settings
-from electricity.models import electricity,Contact
+from electricity.models import electricity, Contact, User as elec_user
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.forms import UserCreationForm
+
+
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    print(User.objects.filter(username="giridhar2339"))
+    #data=User()
+
+
+    return render(request, 'index.html', {})
 
 
 def registeruser(request):
     if not User.objects.filter(username=request.POST["username"]).exists():
-        if request.POST["password"]==request.POST["rpassword"]:
+        if request.POST["password"] == request.POST["rpassword"]:
             if (len(request.POST["password"]) > 7):
-                data=User()
-                data.password=make_password(request.POST["password"])
-                data.first_name=request.POST["first_name"]
-                data.last_name=request.POST["last_name"]
-                data.username=request.POST["username"]
+                data = User()
+                data.password = make_password(request.POST["password"])
+                data.first_name = request.POST["first_name"]
+                data.last_name = request.POST["last_name"]
+                data.username = request.POST["username"]
                 data.email = request.POST["email"]
-                data.area=request.POST["area"]
+                data.area = request.POST["area"]
                 data.is_active = True
                 print("user saved")
-                data.save() 
-                return render(request,"login.html")
+                data.save()
+                return render(request, "login.html")
             else:
-                context="Length must be greter then 8"
-                return render(request,"login.html",{'errorP':context})
+                context = "Length must be greter then 8"
+                return render(request, "login.html", {'errorP': context})
 
         else:
-            context="Password confirmation doesn't match"
+            context = "Password confirmation doesn't match"
             print("wrong password")
-            return render(request,"login.html",{'errorPC':context})
+            return render(request, "login.html", {'errorPC': context})
 
     else:
-        context="Email Already Exist"
-        return render(request,"login.html",{'errorE':context})
+        context = "Email Already Exist"
+        return render(request, "login.html", {'errorE': context})
+
 
 def loginuser(request):
     if request.user.is_authenticated:
         print("Already Logged in")
         return redirect("dashboard")
     else:
-        if(request.method == 'POST'):
+        if (request.method == 'POST'):
             username = request.POST.get('username')
             password = request.POST.get('password')
             user = authenticate(username=username, password=password)
             if user:
                 if user.is_active:
-                    login(request,user)
+                    login(request, user)
                     return redirect('/')
                 else:
-                    return render(request,"login.html",{"err":True})
+                    return render(request, "login.html", {"err": True})
             else:
                 print("Someone tried to login and failed.")
-                print("They used username: {} and password: {}".format(username,password))
-                context="Wrong password or email"
-                return render(request,"login.html",{"invalid":context})
+                print("They used username: {} and password: {}".format(username, password))
+                context = "Wrong password or email"
+                return render(request, "login.html", {"invalid": context})
         else:
             return render(request, 'login.html')
 
+
 def forgotp(request):
-    if(request.method=="POST"):
+    if (request.method == "POST"):
         if User.objects.filter(username=request.POST["username"]).exists():
-            user= User.objects.filter(username=request.POST["username"])
+            user = User.objects.filter(username=request.POST["username"])
             for object in user:
-                if object.first_name==request.POST["first_name"]:
-                    if object.last_name==request.POST["last_name"]:
-                        if request.POST["password"]==request.POST["rpassword"]:
-                            object.password=make_password(request.POST["password"])
+                if object.first_name == request.POST["first_name"]:
+                    if object.last_name == request.POST["last_name"]:
+                        if request.POST["password"] == request.POST["rpassword"]:
+                            object.password = make_password(request.POST["password"])
                             object.save()
                             print('done')
-                            messages.success(request,"Password Changed")
+                            messages.success(request, "Password Changed")
                             return redirect('/login/')
                         else:
-                            context="Password confirmation doesn't match"
-                            return render(request,'forgotpassword.html',{'ErrorFP':context})
+                            context = "Password confirmation doesn't match"
+                            return render(request, 'forgotpassword.html', {'ErrorFP': context})
                     else:
-                        context="Wrong First Name"
-                        return render(request,'forgotpassword.html',{'ErrorFN':context})
+                        context = "Wrong First Name"
+                        return render(request, 'forgotpassword.html', {'ErrorFN': context})
                 else:
-                    context="Wrong Last Name"
-                    return render(request,'forgotpassword.html',{'ErrorLN':context})
+                    context = "Wrong Last Name"
+                    return render(request, 'forgotpassword.html', {'ErrorLN': context})
         else:
-            context="Email not found"
-            return render(request,'forgotpassword.html',{'ErrorEmail':context})
+            context = "Email not found"
+            return render(request, 'forgotpassword.html', {'ErrorEmail': context})
     else:
-        return render(request,'forgotpassword.html')
+        return render(request, 'forgotpassword.html')
+
 
 def logoutuser(request):
     logout(request)
     return redirect('/')
 
+
 @login_required
 def dashboard(request):
-    return render(request,"dashboard.html")
+    return render(request, "dashboard.html")
+
 
 @login_required
 def map(request):
     records = electricity.objects.all()
-    return render(request,"map.html",{"data":records})
+    return render(request, "map.html", {"data": records})
+
 
 @login_required
 def annnouncements(request):
-    return render(request,"announcements.html")
+    return render(request, "announcements.html")
+
 
 @login_required
 def feedback(request):
-    return render(request,"feedback.html")
+    return render(request, "feedback.html")
 
-#Cobtact view
+
+# Cobtact view
 def contact(request):
     if request.method == "POST":
         name = request.POST['name']
@@ -125,21 +139,21 @@ def contact(request):
         phone = request.POST['phone']
         name = request.POST['name']
         message = request.POST['message']
-        contact = Contact(name=name,email=email,phone=phone,message=message)
+        contact = Contact(name=name, email=email, phone=phone, message=message)
         contact.save()
         subject = name + ' wants to contact you'
-        send_mail(subject,message,email,['your_gmail@gmail.com'],fail_silently=False)
-    return render(request,"contact.html")
+        send_mail(subject, message, email, ['your_gmail@gmail.com'], fail_silently=False)
+    return render(request, "contact.html")
+
 
 @csrf_exempt
 def getmapcoordinates(request):
-    if(request.method == 'POST'):
+    if (request.method == 'POST'):
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-        if(len(latitude) and len(longitude)):
+        if (len(latitude) and len(longitude)):
             data = electricity()
             data.latitude = latitude
             data.longitude = longitude
             data.save()
         return redirect('map')
-
